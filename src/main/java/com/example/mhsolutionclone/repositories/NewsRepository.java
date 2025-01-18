8a0b867b1690e3e2dfcacaf819b97c16692671ef
Cuong Nguyen Van <cuong.nv281203@gmail.com>
2025-01-18 11:42:51 +0700

    fix

--- src/main/java/com/example/mhsolutionclone/repositories/NewsRepository.java
+++ src/main/java/com/example/mhsolutionclone/repositories/NewsRepository.java
@@ -11,7 +11,6 @@ import org.jooq.Field;
 import org.jooq.impl.DSL;
 import org.springframework.stereotype.Repository;
 
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
